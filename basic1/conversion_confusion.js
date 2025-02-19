ba9bfc57f6155a807c81a5046cff938568568390
Naitik jain <119112950+Naitik2701@users.noreply.github.com>
2025-02-19 05:50:39 +0000

    conversion of datatype

---
+++ basic1/conversion_confusion.js
@@ -0,0 +1,17 @@
+// let score = "null"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedin = "naitik"
+let isLoggedin = ""
+let booleanIsLoggedin = Boolean(isLoggedin)
+console.log(booleanIsLoggedin);
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
